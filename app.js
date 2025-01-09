3af620bd1336645c3d3101a3fe3f4a750df9b4c0
Hyder Ali <160383412+pearlgem@users.noreply.github.com>
2025-01-09 19:45:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+const inputNumber = document.getElementById("inputNumber");
+
+// function number(x){
+//     inputNumber.value += x
+// // why the parametar and = should be same?
+
+// }
+
+function number(x) {
+    const lastInput = inputNumber.value.slice(-1);
+    if (
+        (lastInput === "+" || lastInput === "-" || lastInput === "*" || lastInput === "/") &&
+        (x === "+" || x === "-" || x === "*" || x === "/")
+    ) {
+        inputNumber.value = inputNumber.value.slice(0, -1) + x;
+    } else {
+        inputNumber.value += x;
+    }
+}
+
+function allClear(){
+    inputNumber.value = "";
+
+}
+
+function caculate(){
+    try{
+    inputNumber.value = eval(inputNumber.value);
+
+}
+catch(error){
+    inputNumber.value = "asa nhi karo"
+}
+
+}
+
+let lastInput = inputNumber.value.slice(-1)
+if (
+    (lastInput === "+" || lastInput === "-" || lastInput === "*" || lastInput === "/") &&
+    (x === "+" || x === "-" || x === "*" || x === "/")
+) {
+    inputNumber.value = inputNumber.value.slice(0, -1) + x;
+}
+ else {
+    inputNumber.value += x;
+}
+
